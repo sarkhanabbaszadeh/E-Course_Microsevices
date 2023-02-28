5af461c9fd91db3b3264356126480f3a0454b66f
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2023-02-28 14:59:55 +0400

    added mongodb nupackages, models

---
+++ Services/Catalog/FreeCourse.Services.Catalog/Models/Course.cs
@@ -0,0 +1,26 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace FreeCourse.Services.Catalog.Models
+{
+	public class Course
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public int Id { get; set; }
+
+        public int Name { get; set; }
+
+		[BsonRepresentation(BsonType.Decimal128)]
+		public decimal Price { get; set; }
+
+        public string UserId { get; set; }
+
+        public string Picture { get; set; }
+
+		[BsonRepresentation(BsonType.DateTime)]
+		public DateTime CreatedTime { get; set; }
+
+
+    }
+}
