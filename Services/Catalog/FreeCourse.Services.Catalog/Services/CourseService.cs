79ca8797b2771c4057631b2b4125061c9b33d34f
Sarkhan Abbaszadeh <sarkhan.abbaszadeh@gmail.com>
2023-03-03 10:15:46 +0400

    iservice added

--- Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
+++ Services/Catalog/FreeCourse.Services.Catalog/Services/CourseService.cs
@@ -7,7 +7,7 @@ using MongoDB.Driver;
 
 namespace FreeCourse.Services.Catalog.Services
 {
-	internal class CourseService
+	internal class CourseService : ICourseService
 	{
 		private readonly IMongoCollection<Course> _courseCollection;
 		private readonly IMongoCollection<Category> _categoryCollection;
@@ -74,6 +74,44 @@ namespace FreeCourse.Services.Catalog.Services
 			}
 
 			return Response<List<CourseDto>>.Success(_mapper.Map<List<CourseDto>>(courses), 200);
+		}
+
+		public async Task<Response<CourseDto>> CreateAsync(CourseCreateDto courseCreateDto)
+		{
+			var newCourse=_mapper.Map<Course>(courseCreateDto);
+
+			newCourse.CreatedTime=DateTime.Now;
+			await _courseCollection.InsertOneAsync(newCourse);
+
+			return Response<CourseDto>.Success(_mapper.Map<CourseDto>(newCourse), 200);
+		}
+
+		public async Task<Response<NoContent>> UpdateAsync(CourseUpdateDto courseUpdateDto)
+		{
+			var updateCourse = _mapper.Map<Course>(courseUpdateDto);
+
+			var result = await _courseCollection.FindOneAndReplaceAsync(x => x.Id == courseUpdateDto.Id, updateCourse);
+
+			if(result==null)
+			{
+				return Response<NoContent>.Fail("Course not found", 404);
+			}
+
+			return Response<NoContent>.Success(204);
+		}
+
+		public async Task<Response<NoContent>> DeleteAsync(string id)
+		{
+			var result = await _courseCollection.DeleteOneAsync(x => x.Id == id);
+
+			if (result.DeletedCount > 0)
+			{
+				return Response<NoContent>.Success(204);
+			}
+			else
+			{
+				return Response<NoContent>.Fail("Course not found", 404);
+			}
 		}
 	}
 }
